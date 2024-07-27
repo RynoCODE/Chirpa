73d53f5ba2d7915e8ab2731ae23a6c9be8a96a0b
RynoCODE <rohit.roychoudhury13@gmail.com>
2024-07-27 21:49:46 +0530

    Database initial commit

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from .info import groq_api_key
-from .models import Question
 from django.http import HttpResponseBadRequest
 from groq import Groq
 import re
+from . models import Question
 personal_prompt="give 1 question based on the language to judge the proficiency of the user in the language ,some topics to be asked compulsorly is oops, dsa , api fetch and decode and many more important topics  no answer required strictly in this format. Strictly use maximum 50 words only for question"
 
 
@@ -35,8 +35,8 @@ client = Groq(
 questions =[]
 
 def question_maker(request):
-    if request.method == 'POST':
-        user_query = request.POST.get('query', '')
+    if request.method == 'GET':
+        user_query = request.GET.get('query', '')
 
     for i in range(5):
         chat_completion = client.chat.completions.create(
@@ -51,6 +51,12 @@ def question_maker(request):
 
         response_content = chat_completion.choices[0].message.content
         questions.append(response_content)
+        question = Question(
+            user = request.user,
+            text = response_content
+        )
+        question.save()
+    
     # questions_list = extract_questions_and_answers(response_content)
     # formatted_questions = [{'text': q} for q in questions_list]
     print(questions)
@@ -71,7 +77,7 @@ def notepad(request):
                 }
             ],
             model="llama3-8b-8192"
-        )
+    )
     
     response_content = chat_completion.choices[0].message.content
 
