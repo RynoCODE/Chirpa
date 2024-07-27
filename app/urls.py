1dc7cdeb88da0d1619d16b9a2a4cbfb13f43bea6
codecxAb <anuragbabu.868@gmail.com>
2024-07-27 21:04:35 +0530

    signup page

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('question_maker/', views.question_maker, name='question_maker'),
     path('index/',views.index),
-    path('notepad/',views.notepad),
+    path('notepad/',views.notepad, name='notepad'),
     path('signin/',views.signin),
+    path('signup/',views.signup),
 ]
--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ import re
 
 
 # llama3 = ChatOpenAI(
-#     api_key=groq_api_key,
+#     api_key=groq_api_key
 #     base_url="https://api.groq.com/openai/v1",
 #     model="llama3-8b-8192",
 # )
@@ -33,28 +33,49 @@ def dashboard(request):
 client = Groq(
     api_key=groq_api_key
 )
-def question_maker(request):
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": f"{personal_prompt} : python"
-            }
-        ],
-        model="llama3-8b-8192"
-    )
+questions =[]
 
-    response_content = chat_completion.choices[0].message.content
-    questions_list = extract_questions_and_answers(response_content)
-    formatted_questions = [{'text': q} for q in questions_list]
-    print(response_content)
-    return render(request, 'question_list.html', {'questions': formatted_questions})
+def question_maker(request):
+    if request.method == 'POST':
+        user_query = request.POST.get('query', '')
+
+    for i in range(5):
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"{personal_prompt} : {user_query}"
+                }
+            ],
+            model="llama3-8b-8192"
+        )
+
+        response_content = chat_completion.choices[0].message.content
+        questions.append(response_content)
+    # questions_list = extract_questions_and_answers(response_content)
+    # formatted_questions = [{'text': q} for q in questions_list]
+    print(questions)
+    return render(request, 'question_list.html', {'questions': questions})
     
 
 def index(request):
     return render(request, 'index.html')
 def notepad(request):
+    chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"{personal_prompt} : {user_query}"
+                }
+            ],
+            model="llama3-8b-8192"
+        )
+    response_content = chat_completion.choices[0].message.content
+
+    
     return render(request, 'notepad.html')
 def signin(request):
     return render(request, 'signin.html')
+def signup(request):
+    return render(request, 'signup.html')
 
